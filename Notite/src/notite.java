cc0d3fd4fca6626782a9cad41a6b96edff4b13af
bajoe9 <marinescu995@gmail.com>
2018-11-16 17:38:05 +0200

    notite

---
+++ Notite/src/notite.java
@@ -0,0 +1,91 @@
+public class notite {public static void main(String[] args) {
+    int myAge;
+    myAge=23;
+    int anAge=myAge;
+    float weight = 90f;
+    double height = 180;
+    long distance = 8987878787136545632l;
+    char gender = 'm';
+    char test = 9;
+    boolean isStudent = false;
+    boolean hasJob = true;
+    int test2 = (int) weight;
+    String mesaj="hello";
+    System.out.print(test);
+    System.out.print(test2);
+    System.out.println(mesaj);
+    System.out.println(myAge);
+
+    int a=1;
+    int b=4;
+    int c=3;
+    int d=(a+b);
+    double a1=1;
+    int b1=4;
+    double d1=(a1+b1);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(d1);
+
+    String mesej1="hi";
+    String mesej2="goodbye";
+    String mesej4 = mesej1 + mesej2;
+    System.out.println(mesej4);
+
+    int x = 10;
+    int y1 = x++;
+    int y2 = ++x;
+    System.out.println(y1);
+    System.out.println(y2);
+
+    boolean noMoreCoffee = false;
+    if(noMoreCoffee);
+    System.out.println("Refill");
+    System.out.println("Make Coffee");
+
+    boolean happy= true;
+
+    if(happy) {
+        System.out.println("clap your hands");
+    }
+    else{System.out.println("dont worry");}
+
+    boolean snowing = true;
+    boolean raining = true;
+    if(snowing){
+        System.out.println("play with snow");
+    }
+    else if (raining){
+        System.out.println("take your umbrella");
+    }
+    else {
+        System.out.println("stay home");
+    }if(snowing){
+        System.out.println("play with snow");
+    }
+    if (raining){
+        System.out.println("take your umbrella");}
+
+
+
+
+    boolean i = false;
+    boolean j = true;
+    if ( i == j){
+        System.out.println("sunt egale");
+    }
+    else {
+        System.out.println("nu sunt egale");
+    }
+
+
+    int p = 0;
+    while (p < 3){
+        System.out.println("incarcare partiala");
+        p++;
+
+
+
+
+    }
+}}
